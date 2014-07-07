318bcf8ad55bb6156477184f08b626df50fc329e
Administrator <Yummy_Chou@163.com>
2014-07-07 23:08:55 +0800

    platform built by yummy

---
+++ src/main/java/com/wisedu/core/common/tools/search/token/CnTokenizer.java
@@ -0,0 +1,17 @@
+package com.wisedu.core.common.tools.search.token;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14-7-7
+ * Time: 下午10:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class CnTokenizer {
+
+    /**
+     * 逆向邻接表
+     */
+    private class AdjacencyList {
+    }
+}
