274229ed38c34f2f970b8dce144537976c6e5758
Yummy <Yummy_Chou@163.com>
2014-07-28 14:32:54 +0800

    mail by yummy

---
+++ src/main/java/com/wisedu/tShow/app/mail/bo/Token.java
@@ -0,0 +1,11 @@
+package com.wisedu.tShow.app.mail.bo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: YUMMY
+ * Date: 14-7-28
+ * Time: 下午2:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class Token {
+}
