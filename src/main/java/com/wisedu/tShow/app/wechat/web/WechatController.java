abc12048d1b743002f1ac25f2b163152c6586088
Yummy <Yummy_Chou@163.com>
2014-11-03 17:56:18 +0800

    wechat by yummy

--- src/main/java/com/wisedu/tShow/app/wechat/web/WechatController.java
+++ src/main/java/com/wisedu/tShow/app/wechat/web/WechatController.java
@@ -1,5 +1,7 @@
 package com.wisedu.tShow.app.wechat.web;
 
+import com.qq.weixin.mp.aes.AesException;
+import com.qq.weixin.mp.aes.WXBizMsgCrypt;
 import com.wisedu.core.utils.PropertyConfigurerUtil;
 import com.wisedu.tShow.tools.wechat.WechatApiDispatcher;
 import com.wisedu.tShow.utils.WechatUtil;
@@ -11,10 +13,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -29,11 +43,143 @@ import java.security.NoSuchAlgorithmException;
 public class WechatController {
     private final static Logger log = LoggerFactory.getLogger(WechatController.class);
 
-    private final static String token = (String)PropertyConfigurerUtil.getProperty("app.wechat.token");
+    // 企业号
+    private static final String qyToken = (String)PropertyConfigurerUtil.getProperty("app.wechat.qy.token");
+    private static final String qyCorpID = (String)PropertyConfigurerUtil.getProperty("app.wechat.qy.CorpID");
+    private static final String qyEncodingAESKey = (String)PropertyConfigurerUtil.getProperty("app.wechat.qy.EncodingAESKey");
 
-    private final static String appId = (String)PropertyConfigurerUtil.getProperty("app.wechat.appId");
+    @RequestMapping(value = "/qy/index.do", method = RequestMethod.GET)
+    public void qyCheck(HttpServletRequest request, HttpServletResponse response){
+        // 加密签名
+        String sVerifyMsgSig = request.getParameter("msg_signature");
+        // 时间戳
+        String sVerifyTimeStamp = request.getParameter("timestamp");
+        // 随机数
+        String sVerifyNonce = request.getParameter("nonce");
+        // 随机字符串
+        String sVerifyEchoStr = request.getParameter("echostr");
 
-    private final static String appSecret = (String)PropertyConfigurerUtil.getProperty("app.wechat.appSecret");
+        String sEchoStr = null;
+        try {
+            WXBizMsgCrypt wxcpt
+                    = new WXBizMsgCrypt(qyToken, qyEncodingAESKey, qyCorpID);
+            sEchoStr = wxcpt.VerifyURL(sVerifyMsgSig, sVerifyTimeStamp, sVerifyNonce, sVerifyEchoStr);
+            response.getWriter().write(sEchoStr);
+        } catch (AesException aese) {
+            log.error(aese.getMessage(), aese);
+        } catch (IOException ioe){
+            log.error(ioe.getMessage(), ioe);
+        }
+    }
+
+    @RequestMapping(value = "/qy/index.do", method = RequestMethod.POST)
+    public void qyProcess(HttpServletRequest request, HttpServletResponse response){
+        // 加密签名
+        String sReqMsgSig = request.getParameter("msg_signature");
+        // 时间戳
+        String sReqTimeStamp = request.getParameter("timestamp");
+        // 随机数
+        String sReqNonce = request.getParameter("nonce");
+        // 随机字符串
+        String sVerifyEchoStr = request.getParameter("echostr");
+
+        String sMsg = null;
+        WXBizMsgCrypt wxcpt = null;
+        try {
+            wxcpt = new WXBizMsgCrypt(qyToken, qyEncodingAESKey, qyCorpID);
+
+            String line = null;
+            StringBuffer sReqData = new StringBuffer();
+            BufferedReader sb = new BufferedReader(new InputStreamReader(request.getInputStream(), "utf-8"));
+            while ((line=sb.readLine()) != null){
+                sReqData.append(line);
+            }
+            sMsg = wxcpt.DecryptMsg(sReqMsgSig, sReqTimeStamp, sReqNonce, sReqData.toString());
+            System.out.println(sMsg);
+        } catch (AesException aese){
+            log.error(aese.getMessage());
+        } catch (IOException ioe){
+            log.error(ioe.getMessage());
+        }
+
+        Document document = null;
+        if (sMsg != null){
+            try {
+                DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                document = db.parse(new InputSource(new StringReader(sMsg)));
+            } catch (ParserConfigurationException pce){
+                log.error(pce.getMessage());
+            } catch (SAXException saxe){
+                log.error(saxe.getMessage());
+            } catch (IOException ioe){
+                log.error(ioe.getMessage());
+            }
+        }
+
+        if (document != null){
+            Element root = document.getDocumentElement();
+            CDATASection toUserNode = (CDATASection)(root.getElementsByTagName("ToUserName").item(0).getChildNodes().item(0));
+            String toUserName = toUserNode.getData();
+
+            CDATASection fromUserNode = (CDATASection)(root.getElementsByTagName("FromUserName").item(0).getChildNodes().item(0));
+            String fromUserName = fromUserNode.getData();
+
+            Node createTimeNode = root.getElementsByTagName("CreateTime").item(0);
+
+            toUserNode.setData(fromUserName);
+            fromUserNode.setData(toUserName);
+            createTimeNode.setTextContent(Integer.toString((int)System.currentTimeMillis()));
+
+            Node msgTypeNode = root.getElementsByTagName("MsgType").item(0);
+            if (msgTypeNode!=null && "text".equals(msgTypeNode.getTextContent())){
+                root.removeChild(root.getElementsByTagName("MsgId").item(0));
+                root.removeChild(root.getElementsByTagName("AgentID").item(0));
+            }
+        }
+
+        String sEncryptMsg = null;
+        if (wxcpt!=null && document!=null){
+            StringWriter sw = new StringWriter();
+            try {
+                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+                StreamResult sr = new StreamResult(sw);
+                transformer.transform(new DOMSource(document.getDocumentElement()), sr);
+
+                sEncryptMsg = wxcpt.EncryptMsg(sr.getWriter().toString(), sReqTimeStamp, sReqNonce);
+                System.out.println(sEncryptMsg);
+            } catch (TransformerConfigurationException tce){
+                log.error(tce.getMessage());
+            } catch (TransformerException tfe){
+                log.error(tfe.getMessage());
+            } catch (AesException aese){
+                log.error(aese.getMessage());
+            } finally {
+                try {
+                    sw.close();
+                } catch (IOException ioe){
+                }
+            }
+        }
+
+        if (sEncryptMsg != null){
+            try {
+                response.setContentType("text/xml");
+                response.getWriter().write(sEncryptMsg);
+            } catch (IOException ioe){
+                log.error(ioe.getMessage());
+            }
+        }
+    }
+
+    // 公众号
+    private final static String mpToken = (String)PropertyConfigurerUtil.getProperty("app.wechat.mp.token");
+    private final static String mpAppId = (String)PropertyConfigurerUtil.getProperty("app.wechat.mp.appId");
+    private final static String mpAppSecret = (String)PropertyConfigurerUtil.getProperty("app.wechat.mp.appSecret");
 
     private static WechatApiDispatcher dispatcher = new WechatApiDispatcher();
 
@@ -42,8 +188,8 @@ public class WechatController {
      * @param request
      * @param response
      */
-    @RequestMapping(value = "/wechat.do", method = RequestMethod.GET)
-    public void check(HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping(value = "/mp/index.do", method = RequestMethod.GET)
+    public void mpCheck(HttpServletRequest request, HttpServletResponse response){
         // 加密签名
         String signature = request.getParameter("signature");
         // 时间戳
@@ -55,7 +201,7 @@ public class WechatController {
 
         try {
             // 签名验证
-            String digest = WechatUtil.checkSignature(token, timestamp, nonce);
+            String digest = WechatUtil.checkSignature(mpToken, timestamp, nonce);
             if (signature.equals(digest)){
                 response.getWriter().write(echostr);
             }
@@ -69,8 +215,8 @@ public class WechatController {
      * @param request
      * @param response
      */
-    @RequestMapping(value = "/wechat.do", method = RequestMethod.POST)
-    public void process(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    @RequestMapping(value = "/mp/index.do", method = RequestMethod.POST)
+    public void mpProcess(HttpServletRequest request, HttpServletResponse response) throws IOException{
         // 加密签名
         String signature = request.getParameter("signature");
         // 时间戳
@@ -81,7 +227,7 @@ public class WechatController {
         String digest = null;
         try {
             // 签名验证
-            digest = WechatUtil.checkSignature(token, timestamp, nonce);
+            digest = WechatUtil.checkSignature(mpToken, timestamp, nonce);
         } catch (NoSuchAlgorithmException nsae){
             log.error(nsae.getMessage());
         }
@@ -111,8 +257,8 @@ public class WechatController {
      * @param request
      * @param model
      */
-    @RequestMapping(value = "/callback.do")
-    private String callback(HttpServletRequest request, ModelMap model){
+    @RequestMapping(value = "/mp/callback.do")
+    private String mpCallback(HttpServletRequest request, ModelMap model){
         model.addAttribute("code", request.getParameter("code"));
         return "wechat/callback";
     }
