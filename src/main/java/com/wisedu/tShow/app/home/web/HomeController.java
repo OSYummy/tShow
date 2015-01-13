79a185177d7d7a7875c52d248cfee83a21bf3838
Yummy <Yummy_Chou@163.com>
2015-01-13 10:18:13 +0800

    platform by yummy

--- src/main/java/com/wisedu/tShow/app/home/web/HomeController.java
+++ src/main/java/com/wisedu/tShow/app/home/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.wisedu.tShow.app.home.web;
 
+import com.wisedu.core.exception.ServiceCode;
+import com.wisedu.core.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -21,6 +23,34 @@ public class HomeController {
     @RequestMapping(value="/index.do")
     public String index(ModelMap model){
         model.addAttribute("greet", "我是首页");
+        return "home/index";
+    }
+
+    @RequestMapping(value="/mgmt.do")
+    public String mgmt(ModelMap model){
+        Integer errCode = null;
+        String errMsg = null;
+        try {
+            model.addAttribute("greet", "我是首页");
+
+            errCode = ServiceCode.SERVICE_OK.getErrCode();
+            errMsg = ServiceCode.SERVICE_OK.getErrMsg();
+        } catch (ServiceException se) {
+            if (se.getErrCode() == null){
+                errCode = se.getErrCode();
+                errMsg = se.getErrMsg();
+            } else {
+                log.error(se.getMessage());
+                errCode = ServiceCode.SERVICE_ERROR.getErrCode();
+                errMsg = ServiceCode.SERVICE_ERROR.getErrMsg();
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            errCode = ServiceCode.SERVICE_ERROR.getErrCode();
+            errMsg = ServiceCode.SERVICE_ERROR.getErrMsg();
+        }
+        model.addAttribute("errCode", errCode);
+        model.addAttribute("errMsg", errMsg);
         return "home/index";
     }
 }
